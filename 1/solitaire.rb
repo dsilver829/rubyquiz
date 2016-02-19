ed8cca24223487289ccd40ec7096f912d07b3bf6
David Silver <dsilver@candidatemetrics.com>
2016-02-19 07:26:15 -0800

    set up input and output

--- 1/solitaire.rb
+++ 1/solitaire.rb
@@ -1,1 +1,11 @@
-puts "Hello"
+#!ruby -w
+
+$deck = (1..52).to_a + ["A","B"]
+
+if ARGV.size == 1 && ARGV[0] =~ /^(?:[A-Z]{5} )*[A-Z]{5}$/
+  puts "DECRYPT"
+elsif ARGV.size == 1
+  puts "ENCRYPT"
+else
+  puts "Usage: solitare.rb MESSAGE"
+end
